3cdb1ebbd2477a1387bdf9f79bac2c468ce631f2
John McCambridge <jmccamb@gmail.com>
2020-08-16 10:45:01 -0500

    Revert "Feature/post"

--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS tech_news_db;
-CREATE DATABASE tech_news_db;
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('express')
-const router = app.Router()
-
-const userRoutes = require('./user-routes')
-const postRoutes = require('./post-routes')
-router.use('/users', userRoutes) // initialize the /users subdomain
-router.use('/posts', postRoutes)
-
-module.exports = router
